8bbe2c41192a560d8d3a48282e82eec22e0327bd
Jonas Deitmerg <jonasdeitmerg@aim.com>
2014-12-23 14:43:10 +0100

    Change power output control, we now have 2 mosfets per connector

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -3,17 +3,34 @@
 
 
 //Pin definitions
-//Upper motor connector
-#define PORT_M1 PORTB
-#define PIOM1   PB2
-#define DDR_M1  DDRB
-#define DDM1    DDB2
+//Upper motor connector high (12V) side
+#define PORT_M1H PORTB
+#define PM1H     PB2
+#define DDR_M1H  DDRB
+#define DDM1H    DDB2
 
-//Lower motor connector
-#define PORT_M2 PORTB
-#define PIOM2   PB1
-#define DDR_M2  DDRB
-#define DDM2    DDB1
+//Upper motor connector low (GND) side
+#define PORT_M1L PORTD
+#define PM1L     PD7
+#define DDR_M1L  DDRD
+#define DDM1L    DDD7
+
+//Lower motor connector high (12V) side
+#define PORT_M2H PORTB
+#define PM2H     PB1
+#define DDR_M2H  DDRB
+#define DDM2H    DDB1
+
+//Lower motor connector low (GND) side
+#define PORT_M2L PORTB
+#define PM2L     PB0
+#define DDR_M2L  DDRB
+#define DDM2L    DDB0
+
+
+//Current state of outputs:
+uint8_t M1_state;
+uint8_t M2_state;
 
 
 //Like the glibc example
@@ -58,8 +75,18 @@ void uart_init(void)
 
 void init(void) {
     //Motors as outputs
-    setbit(DDR_M1, DDM1);
-    setbit(DDR_M2, DDM2);
+    setbit(DDR_M1H, DDM1H);
+    setbit(DDR_M1L, DDM1L);
+    setbit(DDR_M2H, DDM2H);
+    setbit(DDR_M2L, DDM2L);
+
+    setbit(PORT_M1L, PM1L);     //Mosfet on
+    clearbit(PORT_M1H, PM1H);   //Mosfet off
+    M1_state = 0;
+
+    setbit(PORT_M2L, PM2L);     //Mosfet on
+    clearbit(PORT_M2H, PM2H);   //Mosfet off
+    M2_state = 0;
 
     uart_init();
 
@@ -67,32 +94,80 @@ void init(void) {
     //sei();
 }
 
+/*Always use the following functions to set the levels of the output
+ *connector!!
+ */
+
 void M1_low()
 {
     //Set upper connector to 0 volts
-    //There are two mosfets between the pin and the connector
-    clearbit(PORT_M1, PIOM1);
+    //First: make sure it's not set to 12V
+    clearbit(PORT_M1H, PM1H);
+    //Wait for the motor to stop
+    _delay_ms(40);
+    //Then connect it to ground
+    setbit(PORT_M1L, PM1L);
+    M1_state = 0;
 }
 
 void M1_high()
 {
     //Set upper connector to 12 volts
-    //There are two mosfets between the pin and the connector
-    setbit(PORT_M1, PIOM1);
+    //First: make sure it's not set to GND
+    clearbit(PORT_M1L, PM1L);
+    //Wait for the motor to stop
+    _delay_ms(40);
+    //Then connect it to 12V
+    setbit(PORT_M1H, PM1H);
+    M1_state = 1;
+}
+
+void M1_toggle()
+{
+    if(M1_state)
+    {
+        M1_low();
+    }
+    else
+    {
+        M1_high();
+    }
 }
 
 void M2_low()
 {
     //Set lower connector to 0 volts
-    //There are two mosfets between the pin and the connector
-    clearbit(PORT_M2, PIOM2);
+    //First: make sure it's not set to 12V
+    clearbit(PORT_M2H, PM2H);
+    //Wait for the motor to stop
+    _delay_ms(40);
+    //Then connect it to ground
+    setbit(PORT_M2L, PM2L);
+    M2_state = 0;
 }
 
 void M2_high()
 {
     //Set lower connector to 12 volts
-    //There are two mosfets between the pin and the connector
-    setbit(PORT_M2, PIOM2);
+    //First: make sure it's not set to GND
+    clearbit(PORT_M2L, PM2L);
+    //Wait for the motor to stop
+    _delay_ms(40);
+    //Then connect it to 12V
+    setbit(PORT_M2H, PM2H);
+    M2_state = 1;
+}
+
+void M2_toggle()
+{
+    if(M2_state)
+    {
+        M2_low();
+    }
+    else
+    {
+        M2_high();
+    }
 }
 
 void motor_left()
@@ -121,9 +196,33 @@ int main(void)
 
     char c;
 
+    puts("Calfdoor v. 0.1");
+
     while(1)
     {
         c = getchar();
-        putchar(c);
+        switch(c)
+        {
+            case 'l':
+                puts("Motor left");
+                motor_left();
+                break;
+            case 'r':
+                puts("Motor right");
+                motor_right();
+                break;
+            case 's':
+                puts("Motor stop");
+                motor_stop();
+                break;
+            case '1':
+                puts("Toggle M1");
+                M1_toggle();
+                break;
+            case '2':
+                puts("Toggle M2");
+                M2_toggle();
+                break;
+        }
     }
 }
