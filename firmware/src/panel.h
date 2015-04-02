140c7b2dc918e7111130dd873f555170f670c0ca
Jonas Deitmerg <jonasdeitmerg@aim.com>
2015-04-02 14:54:53 +0200

    Add notes about new and old connections, add new files for sensors and panel

---
+++ firmware/src/panel.h
@@ -0,0 +1,13 @@
+#include "common.h"
+
+//status LED
+#define PORT_SLED   PORTB
+#define PSLED       PB0
+#define DDR_SLED    DDRB
+#define DDSLED      DDB0
+
+
+#define PORT_M1  PORTB
+#define PM1      PB0
+#define DDR_M1   DDRB
+#define DDM1     DDB0
