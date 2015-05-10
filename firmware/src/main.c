f269da69e986cb0863586245f93056106e1015ed
Jonas Deitmerg <jonasdeitmerg@aim.com>
2015-05-10 18:05:57 +0200

    Simplify button debouncing and processing

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -7,95 +7,85 @@
 
 #define STATE_AUTO   0x00
 #define STATE_MAN    0x01
-uint8_t state;               //automatic/manual
+uint8_t state;          //automatic/manual
 
-int16_t height;             //actual height of door in motor rotations
+int16_t height;         //actual height of door in motor rotations
 
-uint8_t pressed_buttons;    //1 means pressed for longer than ~20ms
-uint8_t released_buttons;   //0 means not pressed within ~20ms
+//Number of buttons and their bit positions
+#define NBR_BUTTONS 4   //number of buttons
+#define BUTTONS_MR 0    //motor rotation
+#define BUTTONS_ME 1    //manual enable
+#define BUTTONS_MU 2    //manual up
+#define BUTTONS_MD 3    //manual down
+
+//functions to check button states
+uint8_t (*const button_funcs[NBR_BUTTONS])() = {motor_rotation_pressed,
+                                                manual_enable_pressed,
+                                                manual_up_pressed,
+                                                manual_down_pressed};
+
+uint8_t buttons_deb;    //debounced buttons, 1 means pressed
 
 void debounce_buttons()
-//debounces by setting *_buttons only if buttons were/weren't pressed 3 calls
-//in a row
+//debounce buttons by changing bits of buttons_deb maximally every 3rd call
+//(about 25ms)
 {
-    uint8_t buttons;
-    static uint8_t button_hist[3];
+    static uint8_t button_count[NBR_BUTTONS];
     uint8_t i;
 
-    buttons = motor_rotation_pressed();
-    buttons |= manual_enable_pressed() << 1;
-    buttons |= manual_up_pressed() << 2;
-    buttons |= manual_down_pressed() << 3;
-
-    button_hist[0] = button_hist[1];
-    button_hist[1] = button_hist[2];
-    button_hist[2] = buttons;
-
-    //find buttons which were 0 the last 3 times
-    for(i = 0; i < 2; i++)
+    //Loop over all buttons. If its counter is 0, test whether it changed
+    //state.
+    for(i = 0; i < NBR_BUTTONS; ++i)
     {
-        buttons |= button_hist[i];
-    }
-    //only where bit == 0, the last 4 times were 0
-    released_buttons = buttons;
+        if(button_count[i] != 0)
+        {
+            button_count[i]--;
+        }
+        else if(testbit(buttons_deb, i) != button_funcs[i]())
+        {
+            //the counter has expired and the button has toggled, toggle the
+            //appropriate bit in buttons_deb
+            togglebit(buttons_deb, i);
+            //reset counter
+            button_count[i] = 3;
+        }
 
-    //find buttons which were 1 the last 3 times
-    buttons = button_hist[2];
-    for(i = 0; i < 2; i++)
-    {
-        buttons &= button_hist[i];
     }
-    pressed_buttons = buttons;
-    //only where bit == 1, the last 4 times were 1
 }
 
-void next_state()
+static void next_state()
 {
-    if(testbit(pressed_buttons, 1))
-    {
-        //manual enable pressed
-        state = STATE_MAN;
-    }
-    else if(testbit(released_buttons, 1) == 0)
-    {
-        //manual enable not pressed
-        state = STATE_AUTO;
-    }
+    state = testbit(buttons_deb, BUTTONS_ME) ? STATE_MAN : STATE_AUTO;
 }
 
 void process_buttons()
 {
-    static uint8_t prev_rotation_pressed;
+    static uint8_t prev_rotation = 0;
 
-    if(testbit(released_buttons, 0) == 0)
+    if(testbit(buttons_deb, BUTTONS_MR) && !prev_rotation)
     {
-        //motor rotation not pressed
-        prev_rotation_pressed = 0;
-    }
-
-    if(testbit(pressed_buttons, 0))
-    {
-        //motor rotation pressed
-        if(!prev_rotation_pressed)
+        //motor rotation button was pressed (went from 0 to 1)
+        switch(motor_get_direction())
         {
-            switch(motor_get_direction())
-            {
-                case 1:
-                    height++;
-                    break;
-                case 2:
-                    height--;
-                    break;
-            }
+            case 1:
+                height++;
+                break;
+            case 2:
+                height--;
+                break;
+            default:
+                break;
         }
-        prev_rotation_pressed = 1;
     }
+
+    prev_rotation = testbit(buttons_deb, BUTTONS_MR);
+
 
     //process panel buttons
     if(state == STATE_MAN)
     {
-        uint8_t up_pressed = testbit(pressed_buttons, 2);
-        uint8_t down_pressed = testbit(pressed_buttons, 3);
+        uint8_t up_pressed = testbit(buttons_deb, BUTTONS_MU);
+        uint8_t down_pressed = testbit(buttons_deb, BUTTONS_MD);
         if(up_pressed && down_pressed)
         {
             motor_stop();
@@ -119,7 +109,7 @@ void process_buttons()
 ISR(TIMER0_OVF_vect)
 {
     static uint8_t ovf_counter;
-    uint8_t feeding_period;     //wether feeding or waiting for feeding
+    uint8_t feeding_period;     //whether feeding or waiting for feeding
 
     if(ovf_counter++ < 3)   //return 3 out of 4 times, act only every 8ms
     {
