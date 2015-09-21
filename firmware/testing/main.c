f8d23e5ff0f0e9d7ac5e597585c8c0b74164c585
Jonas Deitmerg <jonasdeitmerg@aim.com>
2015-09-21 11:53:14 +0200

    simulation: Put firmware path, mcu into Makefile

--- firmware/testing/main.c
+++ firmware/testing/main.c
@@ -3,7 +3,12 @@
 #include <simavr/avr_ioport.h>
 #include <simavr/sim_elf.h>
 
-#define FIRMW_PATH "bin/main.elf"
+#ifndef FIRMW_PATH
+#error Firmware path not defined!
+#endif
+#ifndef MMCU
+#error MMCU not defined!
+#endif
 
 avr_t *avr = NULL;
 
@@ -14,7 +19,7 @@ int main(int argc, char *argv[])
 
     elf_firmware_t f;
     elf_read_firmware(FIRMW_PATH, &f);
-    avr = avr_make_mcu_by_name(f.mmcu);
+    avr = avr_make_mcu_by_name(MMCU);
     avr_init(avr);
     avr_load_firmware(avr, &f);
 
